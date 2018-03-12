fb326015352b12de6b41476dadc3d4a2d90cdc58
kelly-ke <630409875@qq.com>
2018-03-12 18:26:31 +0800

    数据绑定,事件绑定

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -5,8 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-     date : "Nov 18 2016",
-     title:"正是虾肥蟹壮时"
+    date: "Nov 18 2016",
+    title: "正是虾肥蟹壮时"
   },
 
 
@@ -14,16 +14,31 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var post_content1 ={
-      date:"Sep 18 2016",
-      title:'正是虾肥蟹壮时',
-      post_img:"/images/post/crab.png",
-      content:"菊黄蟹正肥",
-      view_num:"112",
-      collect_num:"96",
-      author_img:"/images/avatar/1.png"
-    }
-    this.setData(post_content1);
+    var posts_content = [
+      {
+        date: "Sep 18 2016",
+        title: '正是虾肥蟹壮时',
+        post_img: "/images/post/crab.png",
+        content: "菊黄蟹正肥",
+        view_num: "18",
+        collect_num: "56",
+        author_img: "/images/avatar/1.png"
+      },
+      {
+        date: "Mar 12 2017",
+        title: '比利林恩的中场战事',
+        post_img: "/images/post/bl.png",
+        content: "李安导演作品",
+        view_num: "112",
+        collect_num: "96",
+        author_img: "/images/avatar/2.png"
+      }
+    ]
+
+
+    this.setData({
+      posts_key: posts_content
+    });
   },
 
   /**
@@ -51,27 +66,27 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
